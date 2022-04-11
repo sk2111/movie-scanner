79a5f55deb49d3edbc0d5eb4d47f0b0894524d87
Sathishkumar Thangavel <sathishkumar.thangavel@chargebee.com>
2022-04-11 12:58:45 +0530

    error cleanup

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const puppeteer = require("puppeteer");
 const express = require("express");
 const app = express();
 const port = 8080;
+const path = require("path");
+
+let browser;
+let page;
+(async () => {
+  browser = await puppeteer.launch();
+  page = await browser.newPage();
+})();
 
 const beastMovieUrl = [
   "https://in.bookmyshow.com/buytickets/beast-coimbatore/movie-coim-ET00311733-MT/20220413", //13 April
@@ -12,7 +20,7 @@ const beastMovieUrl = [
 const theatreNameMatch = "prozone";
 
 const template = {
-  lastUpdated: new Date(),
+  lastUpdated: null,
   error: null,
   theatreInfo: {
     20220413: {
@@ -33,38 +41,45 @@ const template = {
 const response = JSON.parse(JSON.stringify(template));
 
 async function checkMovie(url, theatreNameMatch) {
-  const browser = await puppeteer.launch();
   try {
     const key = url.split("/").pop();
-    const page = await browser.newPage();
     await page.goto(url, { waitUntil: "load" });
     const names = await page.$$eval(".__venue-name", (theatreNames) => {
       return theatreNames.map((tag) => tag.textContent);
     });
+    //console.log("Check names", names);
     const isMatchFound = names.find((name) =>
       name.toLowerCase().includes(theatreNameMatch),
     );
     response.theatreInfo[key].names = names;
     response.theatreInfo[key].isMatchFound = !!isMatchFound;
-    console.log(response);
-    await browser.close();
   } catch (e) {
     response.error = e.message;
     console.log("OOPS ERROR:", e.message);
   }
 }
 
+setInterval(async () => {
+  response.lastUpdated = new Date().toLocaleString();
+  response.error = null;
+  await checkMovie(beastMovieUrl[0], theatreNameMatch);
+  await checkMovie(beastMovieUrl[1], theatreNameMatch);
+  await checkMovie(beastMovieUrl[2], theatreNameMatch);
+}, 10000);
+
+app.get("/", (req, res) => {
+  response.lastUpdated = new Date().toLocaleString();
+  response.error = null;
+  res.sendFile(path.join(__dirname, "/index.html"));
+});
+
 app.get("/movieInfo", (req, res) => {
   try {
-    response.lastUpdated = new Date();
-    response.error = null;
-    beastMovieUrl.forEach((url) => {
-      checkMovie(url, theatreNameMatch);
-    });
+    console.log("Last API Hit time", new Date().toLocaleString());
     res.json(response);
   } catch (e) {
     response.error = e.message;
-    console.log("OOPS ERROR:", e.message);
+    console.log("OOPS ERROR TOP:", e.message);
     res.json(response);
   }
 });
